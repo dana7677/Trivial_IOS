e1d3b0129812702ff6c0a24a6f471958b9810c13
dada <dadad@gmail.com>
2025-01-27 16:43:00 +0100

    Initial Commit

---
+++ TrivialTests/TrivialTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrivialTests.swift
+//  TrivialTests
+//
+//  Created by Tardes on 27/1/25.
+//
+
+import Testing
+@testable import Trivial
+
+struct TrivialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ TrivialUITests/TrivialUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TrivialUITestsLaunchTests.swift
+//  TrivialUITests
+//
+//  Created by Tardes on 27/1/25.
+//
+
+import XCTest
+
+final class TrivialUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
